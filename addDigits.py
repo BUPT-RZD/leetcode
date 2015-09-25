28b2d72b86103dead6400de00472f59ab574da9c
unknown <727516797@qq.com>
2015-09-25 22:15:10 +0800

    leetcode 258

--- addDigits.py
+++ addDigits.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#思路：取各位数字相加，直至相加结果为一位数（<9），考虑边界情况
 class Solution(object):
     def addDigits(self, num):
         a=[]
@@ -13,7 +14,15 @@ class Solution(object):
                 num+=a[i]
             a=[]
         return num
-            
-    
+#思路：规律是直接与9的取余            
+class Solution(object):
+    def addDigits(self, num):
+        if num==0:
+            num=0
+        elif num%9==0:
+            num=9
+        else:
+            num=num%9
+        return num
 
     
