2d5c32fc09f10a6127981a8de1551ea0e44b1039
unknown <727516797@qq.com>
2015-09-25 22:04:32 +0800

    考虑边界条件

--- addDigits.py
+++ addDigits.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
 class Solution(object):
     def addDigits(self, num):
         a=[]
         while num>9:
-            b=num%10
-            a.append(b)
-            num=(num-b)/10        
-            alen = len(a)
+            #考虑边界情况
+            while num >0:
+                b=num%10
+                a.append(b)
+                num=(num-b)/10        
+                alen = len(a)
             for i in range(0,alen):
                 num+=a[i]
             a=[]
