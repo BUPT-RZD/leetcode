3bb6c0af6b647063464780127898694aa50b3b05
unknown <727516797@qq.com>
2015-09-29 10:07:30 +0800

    deleteCode.py

---
+++ deleteCode.py
@@ -0,0 +1,7 @@
+
+class Solution(object):
+    def deleteNode(self, node):
+        if node.next.val == node.val:
+            node.next = node.next.next            
+        else:
+            return self.deleteNode(node.next)
