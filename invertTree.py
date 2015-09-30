8a8e24e89a3189e3c7b1749493bbbe5af94bb1f8
unknown <727516797@qq.com>
2015-09-30 11:42:37 +0800

    leetcode 226

--- invertTree.py
+++ invertTree.py
@@ -1,8 +1,30 @@
+# -*- coding: utf-8 -*-
+#递归
 class Solution(object):
     def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
         if root is None:
             return None
         temp=self.invertTree(root.left)
         root.left = self.invertTree(root.right)
         root.right=temp
         return root
+#非递归
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        stack = [root]  
+        while stack:  
+            node = stack.pop()  
+            if node:  
+                node.left, node.right = node.right, node.left  
+                stack += node.left, node.right  
+        return root  
